c0d0648f7b6ab4c0149c0f9eafc12d90d32f3655
jasonslepicka <jasonslepicka@gmail.com>
2017-02-21 18:48:57 -0800

    Increase sleep time in between elastic search index requests

--- test/test_utils.py
+++ test/test_utils.py
@@ -6,14 +6,15 @@ import time
 def initialize_elasticsearch(documents):
 
     requests.put('http://localhost:9200/dig-sandpaper-test', data="{}")
-    time.sleep(1)
+    time.sleep(5)
 
     for i, document in enumerate(documents):
         url = 'http://localhost:9200/dig-sandpaper-test/ads/{}'.format(i)
         requests.put(url,
                      data=json.dumps(document))
-        time.sleep(1)
+        time.sleep(5)
 
 
 def reset_elasticsearch():
     requests.delete('http://localhost:9200/dig-sandpaper-test')
+    time.sleep(5)
