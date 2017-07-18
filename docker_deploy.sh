645d5f275f745486d75e49df95b1f8b284f31f86
jasonslepicka <jasonslepicka@gmail.com>
2017-07-18 16:15:51 -0700

    Add a /bin/bash directive to docker_deploy.sh

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+echo $DOCKER_USER
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=digsandpaper/digsandpaper
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
