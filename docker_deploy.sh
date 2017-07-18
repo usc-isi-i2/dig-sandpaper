08870b0db9a0fa6f1a224f20eda5528970e679d9
jasonslepicka <jasonslepicka@gmail.com>
2017-07-18 16:42:22 -0700

    Use TRAVIS_COMMIT instead of COMMIT in docker_deploy

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo $DOCKER_USER
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=digsandpaper/digsandpaper
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
+docker build -f Dockerfile -t $REPO:$TRAVIS_COMMIT .
+docker tag $REPO:$TRAVIS_COMMIT $REPO:$TAG
 docker push $REPO
