1a6f9ca5c7d18efd1ea264e2d3ff647eb6b273e1
jasonslepicka <jasonslepicka@gmail.com>
2017-07-17 23:13:41 -0700

    Version 0.1.4-r029

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='digsandpaper',
-    version='0.1.4-r028',
+    version='0.1.4-r029',
     description='digsandpaper',
     author='Jason Slepicka',
     author_email='jasonslepicka@gmail.com',
