5725b8242d11976affd60ed989bb5cf6038e1dd3
jasonslepicka <jasonslepicka@gmail.com>
2017-07-19 16:47:39 -0700

    Deploy development version on commits to master as latest docker

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,6 +2,7 @@
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=digsandpaper/digsandpaper
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -f Dockerfile -t $REPO:$TRAVIS_COMMIT .
+export DOCKERFILE_EXT=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo ".development"; else echo "" ; fi`
+docker build -f Dockerfile${DOCKERFILE_EXT} -t $REPO:$TRAVIS_COMMIT .
 docker tag $REPO:$TRAVIS_COMMIT $REPO:$TAG
 docker push $REPO
