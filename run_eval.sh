eae24dfe20c4644f55c5600ec37ba735a994afe3
jasonslepicka <jasonslepicka@gmail.com>
2017-10-31 19:38:17 -0700

    Fix port for getting config in run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -1,7 +1,7 @@
 TIMESTAMP=`date "+%Y-%m-%d-%H-%M-%S"`
 QUERY_TYPE=${QUERY_TYPE:-pointfact}
 mkdir -p eval/${QUERY_TYPE}/${TIMESTAMP}/
-curl -XGET localhost:9200/config > eval/${QUERY_TYPE}/${TIMESTAMP}/config.json
+curl -XGET localhost:9876/config > eval/${QUERY_TYPE}/${TIMESTAMP}/config.json
 for f in eval/${QUERY_TYPE}/*.json ;
 do
 	bin/query.sh -q $f > eval/${QUERY_TYPE}/$TIMESTAMP/$(basename ${f}).out
