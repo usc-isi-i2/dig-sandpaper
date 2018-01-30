4f1eeda43d88ded022c06a7340c61c5e768c3d4b
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-01-29 17:30:07 -0800

    update total fields limit in es to 10000

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ ES_DSL_VERSION = 'elasticsearch-dsl>={}.0.0,<{}.0.0'.format(ES_MAJOR_VERSION,
 
 setup(
     name='digsandpaper',
-    version='0.1.4-r063',
+    version='0.1.4-r064',
     description='digsandpaper',
     author='Jason Slepicka',
     author_email='jasonslepicka@gmail.com',
