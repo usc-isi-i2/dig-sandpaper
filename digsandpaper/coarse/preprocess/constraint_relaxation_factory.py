6f15dfed3d30d95d8e9eee46541c7e0ac6697ba9
jasonslepicka <jasonslepicka@gmail.com>
2018-03-23 16:44:05 -0700

    Add CombinePredicatesConstraintRelaxer
    Combines all search terms into a single predicate

--- digsandpaper/coarse/preprocess/constraint_relaxation_factory.py
+++ digsandpaper/coarse/preprocess/constraint_relaxation_factory.py
@@ -1,6 +1,52 @@
 __name__ = "ConstraintRelaxation"
 name = __name__
 
+import json
+
+class CombinePredicatesConstraintRelaxer(object):
+
+    name = "CombinePredicatesConstraintRelaxer"
+    component_type = __name__
+
+    def preprocess_filter(self, f):
+        if "clauses" in f:
+            if isinstance(f["clauses"], list):
+                return ' '.join([self.preprocess_filter(c)
+                                for c in f["clauses"]]).strip()
+            elif isinstance(f["clauses"], dict):
+                return ' '.join(self.preprocess_filter(f["clauses"])).strip()
+        else:
+            return f.get("constraint", "")
+
+    def preprocess_clauses(self, clause):
+        constraint = clause.get("constraint", "")
+        constraints = ""
+        filter_constraints = ""
+        if "clauses" in clause:
+            constraints = ' '.join(self.preprocess_clauses(c)
+                                   for c in clause["clauses"]).strip()
+
+        if "filters" in clause:
+            filters = clause["filters"]
+            filter_constraints = ' '.join([self.preprocess_filter(f) for f in filters])
+        return '{} {} {}'.format(constraint, constraints, filter_constraints)
+
+    def preprocess(self, query):
+        where = query["SPARQL"]["where"]
+
+        constraints = self.preprocess_clauses(where)
+        new_clause = {"constraint": constraints,
+                      "isOptional": False,
+                      "predicate": "text",
+                      "type": "owl:Thing",
+                      "id": "123"}
+
+        where["clauses"] = list()
+        where["clauses"].append(new_clause)
+        where["filters"] = list()
+
+        return query
+
 
 class IsOptionalConstraintRelaxer(object):
 
@@ -51,6 +97,8 @@ def get_component(component_config):
     component_name = component_config["name"]
     if component_name == IsOptionalConstraintRelaxer.name:
         return IsOptionalConstraintRelaxer()
+    if component_name == CombinePredicatesConstraintRelaxer.name:
+        return CombinePredicatesConstraintRelaxer()
     else:
         raise ValueError("Unsupported constraint Relaxer {}".
                          format(component_name))
