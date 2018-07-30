299c71cf93c4a0cd9a3c492adf95484f9d2c2e8e
Amandeep Singh <amandeep.s.saggu@gmail.com>
2018-07-30 16:25:02 -0700

    update version in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ ES_DSL_VERSION = 'elasticsearch-dsl>={}.0.0,<{}.0.0'.format(ES_MAJOR_VERSION,
 
 setup(
     name='digsandpaper',
-    version='0.2.0-r003',
+    version='0.2.0-r004',
     description='digsandpaper',
     author='Jason Slepicka',
     author_email='jasonslepicka@gmail.com',
